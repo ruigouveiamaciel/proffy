c3f21c6e293d50f1028d236cecd81cb7ae35e507
Rui Maciel <ruigouveiamaciel@gmail.com>
2020-08-04 11:29:08 +0000

    landing page & study page template

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
+import "./styles.css"
+
+interface TeacherItemProps {
+
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = (props) => {
+    return (
+        <article className="teacher-item">
+            <header>
+                <img src="https://avatars2.githubusercontent.com/u/17518628?s=460&v=4" alt="Rui Maciel"/>
+                <div>
+                    <strong>Rui Maciel</strong>
+                    <span>Química</span>
+                </div>
+            </header>
+
+            <p>
+                Entusiasta das melhores tecnologias de química avançada.
+                <br /><br />
+                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.
+            </p>
+
+            <footer>
+                <p>
+                    Preço/hora
+                    <strong>80,00 €</strong>
+                </p>
+                <button type="button">
+                    <img src={whatsappIcon} alt="Whatsapp"/>
+                    Entrar em contacto
+                </button>
+            </footer>
+        </article>
+    )
+}
+
+export default TeacherItem;
