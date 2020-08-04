c7ce25d6662e30286584712ab000a6addf055b66
Rui Maciel <ruigouveiamaciel@gmail.com>
2020-08-04 13:15:05 +0000

    migrations setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.post("/users", (req, res) => {
+    console.log("acessou a rota");
+
+    res.json({"derp": 1});
+})
+
+app.listen(3333)
