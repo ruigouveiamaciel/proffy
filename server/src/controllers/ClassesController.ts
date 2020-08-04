4aea00daf40dc38814d44af0ecc4037c3e628e14
Rui Maciel <ruigouveiamaciel@gmail.com>
2020-08-04 14:26:06 +0000

    endpoint to list classes

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -9,6 +9,37 @@ interface ScheduleItem {
 }
 
 export default class ClassesController {
+    async index(req: Request, res: Response) {
+        const filters = req.query;
+
+        if (!filters.week_day || !filters.subject || !filters.time) {
+            return res.status(400).json({
+                 error: "Missing filters to search classes"
+            })
+        }
+
+        const week_day = filters.week_day as string;
+        const subject = filters.subject as string;
+        const time = filters.time as string;
+
+        const timeInMinutes = convertHourToMinutes(time)
+
+        const classes = await db("classes")
+            .whereExists(function() {
+                this.select("class_schedule.*")
+                    .from("class_schedule")
+                    .whereRaw("`class_schedule`.`class_id` = `classes`.`id`")
+                    .whereRaw("`class_schedule`.`week_day` = ??", [Number(week_day)])
+                    .whereRaw("`class_schedule`.`from` <= ??", [timeInMinutes])
+                    .whereRaw("`class_schedule`.`to` > ??", [timeInMinutes])
+            })
+            .where("classes.subject", "=", subject)
+            .join("users", "classes.user_id", "=", "users.id")
+            .select(["classes.*", "users.*"]);
+
+        return res.status(200).json(classes)
+    }
+
     async create(req: Request, res: Response) {
         const {
             name, avatar, whatsapp, bio, subject, cost, schedule
